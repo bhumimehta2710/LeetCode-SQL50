16c8bfb7050d5f45b051f298e8513cf1823aed41
bhumimehta2710 <54493146+bhumimehta2710@users.noreply.github.com>
2024-08-28 23:52:16 -0400

    Create 1757. Recyclable and Low Fat Products.sql

---
+++ Select/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,5 @@
+SELECT product_id 
+FROM Products 
+WHERE low_fats='Y' AND recyclable = 'Y';
+
+-- The above query filters the 'Products' table using the 'WHERE' clause and the 'AND' operator to find products that are both low fat and recyclable, based on the specified criteria.
