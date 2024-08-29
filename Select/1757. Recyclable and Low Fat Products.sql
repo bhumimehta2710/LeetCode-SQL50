6e519f299d52c38db2bf47d198b82db7e00cbe4e
bhumimehta2710 <54493146+bhumimehta2710@users.noreply.github.com>
2024-08-29 00:22:25 -0400

    Update 1757. Recyclable and Low Fat Products.sql

--- Select/1757. Recyclable and Low Fat Products.sql
+++ Select/1757. Recyclable and Low Fat Products.sql
@@ -3,5 +3,3 @@ FROM Products
 WHERE low_fats='Y' AND recyclable = 'Y';
 
 -- The above query filters the 'Products' table using the 'WHERE' clause and the 'AND' operator to find products that are both low fat and recyclable, based on the specified criteria.
-
-[Link to this Solution](https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50)
